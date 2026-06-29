1dedd72fffb660836f61086be64f1801ae81ce7a
luvtomar2005 <luvtomar2005@gmail.com>
2026-06-29 20:28:59 +0530

    Complete Week 3 Exercise 2 - Implement setter dependency injection

--- Week-3/Exercise-1-LibraryManagement/src/main/java/com/library/MainApp.java
+++ Week-3/Exercise-1-LibraryManagement/src/main/java/com/library/MainApp.java
@@ -1,28 +1,20 @@
 package com.library;
 
-import com.library.repository.BookRepository;
-import com.library.service.BookService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.service.BookService;
 
 public class MainApp {
 
     public static void main(String[] args) {
 
-        // Load the Spring IoC Container
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        // Retrieve BookRepository bean
-        BookRepository repository =
-                context.getBean("bookRepository", BookRepository.class);
-
-        // Retrieve BookService bean
         BookService service =
                 context.getBean("bookService", BookService.class);
 
-        // Call methods
-        repository.saveBook();
         service.displayService();
     }
 }
