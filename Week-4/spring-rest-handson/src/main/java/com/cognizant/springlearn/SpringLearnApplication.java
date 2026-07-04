7fec1741e13644a1274f5b7c1d7d87eeaab3b645
Luv Tomar <luvtomar2005@gmail.com>
2026-07-04 21:08:50 +0530

    Complete Week 4 Hands-on 4: Load Country bean from Spring XML

---
+++ Week-4/spring-rest-handson/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,22 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringLearnApplication.class, args);
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = context.getBean("country", Country.class);
+
+        System.out.println(country);
+    }
+}
