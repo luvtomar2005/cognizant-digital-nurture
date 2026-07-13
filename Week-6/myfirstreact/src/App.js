1a69213400804252e739b3c08e8f917ba3eaf809
Luv Tomar <luvtomar2005@gmail.com>
2026-07-14 02:39:25 +0530

    Rename Weekl 6 folder to Week-6

---
+++ Week-6/myfirstreact/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "40px",
+      }}
+    >
+      <h1>Welcome the first session of React</h1>
+    </div>
+  );
+}
+
+export default App;
