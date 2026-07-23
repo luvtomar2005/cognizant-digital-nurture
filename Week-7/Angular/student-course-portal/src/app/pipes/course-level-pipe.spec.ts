f86bcceef46b7dd4f98236c65380fd5001311974
Luv Tomar <luvtomar2005@gmail.com>
2026-07-23 12:37:54 +0530

    chore(week-7): clean up project structure

--- Week-7/Angular/student-course-portal/src/app/pipes/course-level-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CourseLevelPipe } from './course-level-pipe';
-
-describe('CourseLevelPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CourseLevelPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
